ca6a2ab60c2a1abc4a36f5f46c0c9c299f66c907
Nicolò Vescera <slayercandy@icloud.com>
2015-04-13 09:45:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Definisci le costanti
 #define N 3
@@ -13,10 +14,12 @@ struct s_studente{
 };
 // Definisci studente come struct s_studente
 typedef struct s_studente studente;
+void ordina();
 int main(int argc, char** argv) {
     // Dichiara le variabili locali
-    int i;
+    int i,j;
     studente elenco[N];
+    char temp[MAX_STRLEN];
     // Codice per l'inserimento, da parte dell'utente, dei dati
     for(i=0;i<N;i++){
         printf("Inserisci il nome della %d persona: ",i+1);
@@ -28,10 +31,18 @@ int main(int argc, char** argv) {
         printf("Inserisci la classe della %d persona: ",i+1);
         scanf("%s",(elenco[i].class));
     }
+    for(i=0;i<N-1;i++){
+        for(j=0;j<N-1;j++){
+            if(strcmp(elenco[j+1].surname,elenco[j].surname)>0){
+                strcpy(temp,elenco[j+1].surname);
+                strcpy(elenco[j+1].surname,elenco[j].surname);
+                strcpy(elenco[j].surname,temp);
+            }
+        }
+    }
     // Codice per la stampa dei dati (uno studente per riga)
     for(i=0;i<N;i++){
         printf("%s; %s; %d; %s;\n",elenco[i].name,elenco[i].surname,elenco[i].age,elenco[i].class);
     }
     return (EXIT_SUCCESS);
 }
-
